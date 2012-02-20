d7aa17ba14e1f1d7c14f7cef1b8a9778b9583438
Garry Hill <garry@magnetised.info>
2012-02-20 18:24:50 +0000

    Release 0.3.1

--- lib/simultaneous.rb
+++ lib/simultaneous.rb
@@ -4,7 +4,7 @@ require 'socket'
 require 'eventmachine'
 
 module Simultaneous
-  VERSION = "0.3.0"
+  VERSION = "0.3.1"
 
   DEFAULT_CONNECTION = "/tmp/simultaneous-server.sock"
   DEFAULT_PORT = 9999
